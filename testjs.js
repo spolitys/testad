ab9d792e58a822c50d9fca16ef5aadb2141edbc7
shirouo <111714642+shirouo@users.noreply.github.com>
2023-07-26 08:53:04 -0400

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,43 +1,3 @@
-/**
-* @name Spolitys modifier
-* @version 0.0.1
-* @description Spolitys custom client side profile changer
-* @author Spolity
-* @authorId 424759131036188672
-* @source https://raw.githubusercontent.com/shirouo/testad/main/testjs.js
-* @updateUrl https://raw.githubusercontent.com/shirouo/testad/main/testjs.js
-*/
-
-// me
-const spol_display_name = "Spolity 🐈‍⬛";
-const spol_username = "noirleeap";
-
-// seo
-const seos_username = "seo";
-
-// my sister
-const lunas_username = "luna";
-
-module.exports = class spolity {
-load() {
-}
-start() {
-let _mods = webpackChunkdiscord_app.push([[Symbol()],{},({c})=>Object.values(c)]);
-webpackChunkdiscord_app.pop();
-
-const spolitysetprop = (...props) => {
-    for (let m of _mods) {
-        try {
-            if (!m.exports || m.exports === window) continue;
-            if (props.every((x) => m.exports?.[x])) return m.exports;
-
-            for (let ex in m.exports) {
-                if (props.every((x) => m.exports?.[ex]?.[x])) return m.exports[ex];
-            }
-        } catch {}
-    }
-}
-setInterval(function () {
 //-- SPOLITY 
 
 // spol bot tag
@@ -60,10 +20,3 @@ setInterval(function () {
 //-- LUNA
 // my sisters username
  spolitysetprop("getUserStoreVersion").getUser("424725601597915137").username = lunas_username;
-}, 1000);
-}
-stop() {
-
-}
-}
-/*@end@*/ 
