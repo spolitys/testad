584349868d34a626a8b831dfbcd57d6a9bd3b224
shirouo <111714642+shirouo@users.noreply.github.com>
2023-07-26 05:40:59 -0400

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,3 +1,41 @@
+/**
+* @name Spolity
+* @version 0.0.1
+* @description Spolitys custom client side profile changer
+* @author spolity
+*
+*/
+
+// me
+const spol_display_name = "Spolity 🐈‍⬛";
+const spol_username = "noirleeap";
+
+// seo
+const seos_username = "seo";
+
+// my sister
+const lunas_username = "luna";
+
+module.exports = class spolity {
+load() {
+}
+start() {
+let _mods = webpackChunkdiscord_app.push([[Symbol()],{},({c})=>Object.values(c)]);
+webpackChunkdiscord_app.pop();
+
+const spolitysetprop = (...props) => {
+    for (let m of _mods) {
+        try {
+            if (!m.exports || m.exports === window) continue;
+            if (props.every((x) => m.exports?.[x])) return m.exports;
+
+            for (let ex in m.exports) {
+                if (props.every((x) => m.exports?.[ex]?.[x])) return m.exports[ex];
+            }
+        } catch {}
+    }
+}
+setInterval(function () {
 //-- SPOLITY 
 
 // spol bot tag
@@ -20,3 +58,10 @@
 //-- LUNA
 // my sisters username
  spolitysetprop("getUserStoreVersion").getUser("424725601597915137").username = lunas_username;
+}, 1000);
+}
+stop() {
+
+}
+}
+/*@end@*/ 
