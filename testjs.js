5e049bca5c4610818f9fd3688d149f39c8de29f9
shirouo <111714642+shirouo@users.noreply.github.com>
2023-07-26 05:36:18 -0400

    Create testjs.js

---
+++ testjs.js
@@ -0,0 +1,22 @@
+//-- SPOLITY 
+
+// spol bot tag
+// spolitysetprop("getUserStoreVersion").getUser("424759131036188672").bot = true;
+// spolitysetprop("getUserStoreVersion").getUser("424759131036188672").flags = 65536;
+// spol ai tag
+// spolitysetprop("getUserStoreVersion").getUser("424759131036188672").isClyde = () => true;;
+
+// spol display name
+ spolitysetprop("getUserStoreVersion").getUser("424759131036188672").global_name = spol_display_name;
+// spol username
+ spolitysetprop("getUserStoreVersion").getUser("424759131036188672").username = spol_username;
+
+
+//-- SEO
+// seos username
+ spolitysetprop("getUserStoreVersion").getUser("908160886886244412").username = seos_username;
+
+
+//-- LUNA
+// my sisters username
+ spolitysetprop("getUserStoreVersion").getUser("424725601597915137").username = lunas_username;
